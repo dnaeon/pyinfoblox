278d2ae74602084f800c0ee4be51046a20336714
Marin Atanasov Nikolov <dnaeon@gmail.com>
2014-11-29 16:17:29 +0200

    Commit setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='pyinfoblox',
+      version='0.1.0',
+      description='Infoblox WAPI module for Python',
+      long_description=open('README.rst').read(),
+      author='Marin Atanasov Nikolov',
+      author_email='dnaeon@gmail.com',
+      license='BSD',
+      url='https://github.com/dnaeon/pyinfoblox',
+      download_url='https://github.com/dnaeon/pyinfoblox/releases',
+      packages=['pyinfoblox'],
+      install_requires=[
+        'requests >= 2.4.3',
+      ]
+)

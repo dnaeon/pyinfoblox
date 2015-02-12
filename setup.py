fbb1349a250acb3b5ef11537e38ea356b5856d40
Marin Atanasov Nikolov <dnaeon@gmail.com>
2015-02-12 10:44:20 +0200

    pyinfoblox version 0.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyinfoblox',
-      version='0.1.0',
+      version='0.1.1',
       description='Infoblox WAPI module for Python',
       long_description=open('README.rst').read(),
       author='Marin Atanasov Nikolov',

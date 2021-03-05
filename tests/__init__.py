0d872fa0a4b7ae71c60e54557a8b72aa44603ebc
James H <jamesh3450@gmail.com>
2021-03-05 00:17:14 -0800

    fix data loading: add json loader to file handler context, add class inheritence

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,25 +1,28 @@
 import json
 import unittest
 from os import getenv
+from unittest import TestCase
 from pyinfoblox import InfobloxWAPI
 
-class BaseTestCase():
+class BaseTestCase(TestCase):
 
-    def __init__(self):
-        super().__init__()
-        self.infoblox = InfobloxWAPI(
-                username=getenv("INFOBLOX_USERNAME"),
-                password=getenv("INFOBLOX_PASSWORD"),
-                wapi=getenv("INFOBLOX_URL_DEV"),
-                verify=False)
-        
-        with open('./test_data/host.json') as jsonfile:
+    def __init__(self, *args, **kwargs):
+        super(BaseTestCase, self).__init__(*args, **kwargs)
+        self.infoblox = InfobloxWAPI(username=getenv("INFOBLOX_USERNAME"),
+                                     password=getenv("INFOBLOX_PASSWORD"),
+                                     wapi=getenv("INFOBLOX_URL_DEV"),
+                                     verify=False)
+
+        # load the data for testing
+        with open('./tests/test_data/host.json') as jsonfile:
             self.host_base = json.load(jsonfile)
-        with open('./test_data/host_update.json') as jsonfile:
+        with open('./tests/test_data/host_update.json') as jsonfile:
             self.host_base_update = json.load(jsonfile)
-        # with open('./test_data/network.json') as jsonfile:
-        #     self.network_base = json.load(jsonfile)
-        # with open('./test_data/subnet.json') as jsonfile:
-        #     self.subnet_base = json.load(jsonfile)
-        
-        
+        with open('./tests/test_data/network_container.json') as jsonfile:
+            self.network_container_base = json.load(jsonfile)
+        with open('./tests/test_data/network_container_update.json') as jsonfile:
+            self.network_container_base_update = json.load(jsonfile)
+        with open('./tests/test_data/network.json') as jsonfile:
+            self.network_base = json.load(jsonfile)
+        with open('./tests/test_data/network_update.json') as jsonfile:
+            self.network_base_update = json.load(jsonfile)
